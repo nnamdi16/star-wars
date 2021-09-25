5376764a047a4c9324edd2350deb73b455fd6281
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2021-09-25 23:40:49 +0100

    chore(deployment):set up cors and swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,44 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+} from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
+import * as helmet from 'helmet';
+import {
+  ClassSerializerInterceptor,
+  Logger,
+  ValidationPipe,
+} from '@nestjs/common';
 
 const port = process.env.PORT;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(port || 4000);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: ['debug', 'error', 'log', 'verbose', 'warn'],
+  });
+  app.use(cookieParser());
+  app.enableCors();
+  app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  const config = new DocumentBuilder()
+    .setTitle('Iexchange Backend Services')
+    .setDescription('REST API sercices for Iexchange')
+    .setVersion('1.0')
+    // .addTag("iexchange")
+    .addBearerAuth()
+    .addCookieAuth('optional-session-id')
+    .build();
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(port);
+  Logger.log(`Server started on http://localhost:${port}, Boostrap`);
 }
 bootstrap();
