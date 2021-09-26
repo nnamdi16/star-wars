a4966933393029919d9fb1fd931295853d9accd2
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2021-09-26 14:17:24 +0100

    chore(movies-api): Added ConfigModule and made it global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [MoviesModule, HttpModule],
+  imports: [MoviesModule, HttpModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
