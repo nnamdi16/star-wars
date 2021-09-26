13992c4367283cb096ed1b2be2bc581ad50adca6
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2021-09-26 12:50:24 +0100

    feat(movie): created movies resources and added HttpModule to movies module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [],
+  imports: [MoviesModule, HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
