b9d8d2f545d4db3acfbb801b7de304b2e8d46ab2
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2021-09-26 06:26:44 +0100

    Updated Swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import {
   SwaggerDocumentOptions,
 } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
-import * as helmet from 'helmet';
+
 import {
   ClassSerializerInterceptor,
   Logger,
@@ -22,14 +22,12 @@ async function bootstrap() {
   });
   app.use(cookieParser());
   app.enableCors();
-  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   const config = new DocumentBuilder()
-    .setTitle('Iexchange Backend Services')
-    .setDescription('REST API sercices for Iexchange')
+    .setTitle('Star Wars Movies Backend Services')
+    .setDescription('REST API backend services for Star Wars ')
     .setVersion('1.0')
-    // .addTag("iexchange")
     .addBearerAuth()
     .addCookieAuth('optional-session-id')
     .build();
