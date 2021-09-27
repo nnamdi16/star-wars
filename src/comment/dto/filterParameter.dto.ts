ae5d4d60259a808deda74062966fc8e761b398f4
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2021-09-27 15:59:52 +0100

    Implemented QueryParams, Order and Gender enum

---
+++ src/comment/dto/filterParameter.dto.ts
@@ -0,0 +1,16 @@
+export enum QueryParams {
+  NAME = 'name',
+  GENDER = 'gender',
+  HEIGHT = 'height',
+}
+
+export enum Order {
+  ASCENDING = 'ascending',
+  DESCENDING = 'descending',
+}
+
+export enum Gender {
+  MALE = 'male',
+  FEMALE = 'female',
+  NOT_APPLICABLE = 'n/a',
+}
