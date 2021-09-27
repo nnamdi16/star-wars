24f392470c159e67bfd5d895a0e9da687b707bbf
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2021-09-27 15:56:45 +0100

    set up database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,29 @@
+import { CommentsModule } from './movies/comments.module';
+import { DatabaseModule } from './database/database.module';
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MoviesModule } from './movies/movies.module';
+import * as Joi from '@hapi/joi';
 
 @Module({
-  imports: [MoviesModule, HttpModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    CommentsModule,
+    HttpModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        POSTGRES_HEROKU_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_HEROKU_USER: Joi.string().required(),
+        POSTGRES_HEROKU_PASSWORD: Joi.string().required(),
+        POSTGRES_HEROKU_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
