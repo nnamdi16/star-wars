2f206e7eae7f0366d9e7308281fdab8fda0a4b72
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2021-09-27 16:04:19 +0100

    Added commentsModule and exported MovieService to movie module

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,11 +1,13 @@
-import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { MoviesController } from './movies.controller';
+import { HttpModule } from '@nestjs/axios';
+import { CommentsModule } from '../comment/comments.module';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, CommentsModule],
   controllers: [MoviesController],
   providers: [MoviesService],
+  exports: [MoviesService],
 })
 export class MoviesModule {}
