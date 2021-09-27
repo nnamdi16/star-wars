e0bf6d57547ffdd8569fd5fda55a509549c0540c
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2021-09-27 15:59:13 +0100

    Implemented create comments dto

---
+++ src/comment/dto/create.comments.dto.ts
@@ -0,0 +1,47 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsIP,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateCommentDto implements Readonly<CreateCommentDto> {
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    description: 'Comments about the movie',
+    example: 'Interesting Movie',
+    nullable: false,
+    required: true,
+    title: 'comment',
+  })
+  public comment: string;
+
+  @IsIP()
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(500)
+  @ApiProperty({
+    description: 'IP of the commenter',
+    example: '192.0. 2.146',
+    nullable: false,
+    required: true,
+    title: 'ipAddress',
+  })
+  public ipAddress: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  @ApiProperty({
+    description: 'Movie Id',
+    example: 1,
+    nullable: false,
+    required: true,
+    title: 'movieId',
+  })
+  public movieId: number;
+}
